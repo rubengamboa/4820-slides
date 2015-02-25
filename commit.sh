eab87ae519dcd80feb24905cad8d1d25d5e3a148
Ruben Gamboa <ruben@uwyo.edu>
2015-02-25 11:36:23 -0700

    Changed definition of superkey to agree with book

--- commit.sh
+++ commit.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+if [ "$1" == "" ]
+then
+    echo "Usage: commit.sh commit message"
+    exit 1
+fi
+
 git add *
-git commit -m "Latest updates"
-git push origin master
+git commit -m "$*"
 git push origin gh-pages
+git checkout master
+git merge gh-pages
+git push origin master
+git checkout gh-pages
